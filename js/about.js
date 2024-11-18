e274762f90068597dd034a772bd1fe9d02f1cc31
byebye-jisoo <hanyoung0410@naver.com>
2024-11-18 16:51:10 +0900

    11/18 16:51

--- js/about.js
+++ js/about.js
@@ -3,23 +3,28 @@ const content = document.getElementsByClassName("content");
 let page = 0; // 영역 포지션 초기값
 const lastPage = content.length - 1; // 마지막 페이지
 
+window.onload = () => {
+  wrap.style.top = "0vh";
+};
+
 window.addEventListener(
   "wheel",
   (e) => {
-    e.preventDefault();
+    e.preventDefault(); // 기본 스크롤 이벤트 방지
     if (e.deltaY > 0) {
-      page++;
+      page++; // 아래로 스크롤
     } else if (e.deltaY < 0) {
-      page--;
+      page--; // 위로 스크롤
     }
 
+    // 페이지 범위 제한
     if (page < 0) {
       page = 0;
     } else if (page > lastPage) {
       page = lastPage;
     }
-    console.log(e.deltaY);
-    wrap.style.top = page * -100 + "vh";
+
+    wrap.style.top = `-${page * 81.4}vh`; // 각 세션 높이만큼 이동
   },
   { passive: false }
 ); // 디폴트 기능 제거 - 스크롤
