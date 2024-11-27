d516c3981560d66724b856757b9eb9bad1293360
byebye-jisoo <hanyoung0410@naver.com>
2024-11-28 02:49:23 +0900

    .

--- js/about.js
+++ js/about.js
@@ -1,8 +1,7 @@
-const wrap = document.getElementsByClassName("wrap")[0]; // 보일 영역
+const wrap = document.getElementsByClassName("wrap")[0]; // 스크롤 영역
 const content = document.getElementsByClassName("content");
 let page = 0; // 현재 페이지
 const lastPage = content.length - 1; // 마지막 페이지
-
 let isScrolling = false; // 스크롤 이벤트 중복 방지 플래그
 
 window.onload = () => {
@@ -12,12 +11,11 @@ window.onload = () => {
 window.addEventListener(
   "wheel",
   (e) => {
-    if (isScrolling) return; // 스크롤 제한 중이면 이벤트 무시
-
-    e.preventDefault(); // 기본 스크롤 이벤트 방지
+    if (isScrolling) return; // 스크롤 중복 방지
+    e.preventDefault();
 
     isScrolling = true; // 스크롤 시작
-    setTimeout(() => (isScrolling = false), 500); // 700ms 후 스크롤 가능
+    setTimeout(() => (isScrolling = false), 500); // 500ms 후 스크롤 가능
 
     if (e.deltaY > 0) {
       page++; // 아래로 스크롤
@@ -26,14 +24,11 @@ window.addEventListener(
     }
 
     // 페이지 범위 제한
-    if (page < 0) {
-      page = 0;
-    } else if (page > lastPage) {
-      page = lastPage;
-    }
+    if (page < 0) page = 0;
+    if (page > lastPage) page = lastPage;
 
-    // 페이지 이동
-    wrap.style.top = `-${page * 81.4}vh`; // 각 세션 높이만큼 이동
+    // 페이지 이동 (100%로 이동 계산)
+    wrap.style.top = `-${page * 100}vh`;
   },
   { passive: false }
 );
